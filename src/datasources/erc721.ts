350a3261897c7d24e5d444287192ce6816a041dc
qwerteum <ys-kim@snu.ac.kr>
2022-06-24 14:20:34 +0900

    modified subgraph for collection tracker

--- src/datasources/erc721.ts
+++ src/datasources/erc721.ts
@@ -1,101 +1,220 @@
-import {
-	Address,
-} from '@graphprotocol/graph-ts'
+import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
+import { Blockchain, Collection, Owner, Token, Transaction } from "../../generated/schema";
+import { Transfer } from "../../generated/erc721/EIP721";
+import { EIP721 } from "../../generated/erc721/EIP721";
 
-import {
-	ERC721Transfer,
-} from '../../generated/schema'
 
-import {
-	Approval       as ApprovalEvent,
-	ApprovalForAll as ApprovalForAllEvent,
-	Transfer       as TransferEvent,
-} from '../../generated/erc721/IERC721'
+export const ZERO_BI = BigInt.zero();
+export const ONE_BI = BigInt.fromI32(1);
+export const TWO_BI = BigInt.fromI32(2);
+export const THREE_BI = BigInt.fromI32(3);
+export const FOUR_BI = BigInt.fromI32(4);
+export const ZERO_BD = BigDecimal.zero();
+export const ONE_BD = BigDecimal.fromString("1");
+export const ZERO_ADDRESS = Address.fromString("0x0000000000000000000000000000000000000000");
+export const ONE_ETHER_IN_WEI = BigInt.fromI32(10).pow(18);
 
-import {
-	events,
-	transactions,
-} from '@amxx/graphprotocol-utils'
+export function handleTransfer(event: Transfer): void {
+  let blockchain = Blockchain.load("ETH");
+  if (blockchain === null) {
+    // Blockchain
+    blockchain = new Blockchain("ETH");
+    blockchain.totalCollections = BigInt.zero();
+    blockchain.totalTokens = BigInt.zero();
+    blockchain.totalTransactions = BigInt.zero();
+    blockchain.save();
+  }
+  blockchain.totalTransactions = blockchain.totalTransactions.plus(BigInt.fromI32(1));
+  blockchain.save();
 
-import {
-	fetchAccount,
-} from '../fetch/account'
+  let collection = Collection.load(event.address.toHex());
+  if (collection === null) {
+    // Collection
+    collection = new Collection(event.address.toHex());
+    collection.name = fetchName(event.address);
+    collection.symbol = fetchSymbol(event.address);
+	collection.totalOwners = BigInt.zero(); // totalOwners Introduction
+    collection.totalTokens = BigInt.zero();
+    collection.totalTransactions = BigInt.zero();
+    collection.block = event.block.number;
+    collection.createdAt = event.block.timestamp;
+    collection.updatedAt = event.block.timestamp;
+    collection.save();
 
-import {
-	fetchERC721,
-	fetchERC721Token,
-	fetchERC721Operator,
-} from '../fetch/erc721'
+    // Blockchain
+    blockchain.totalCollections = blockchain.totalCollections.plus(BigInt.fromI32(1));
+    blockchain.save();
+  }
+  collection.totalTransactions = collection.totalTransactions.plus(BigInt.fromI32(1));
+  collection.updatedAt = event.block.timestamp;
+  collection.save();
 
-export function handleTransfer(event: TransferEvent): void {
-	let contract = fetchERC721(event.address)
-	if (contract != null) {
-		let token = fetchERC721Token(contract, event.params.tokenId)
-		let from  = fetchAccount(event.params.from)
-		let to    = fetchAccount(event.params.to)
+  let from = Owner.load(event.params.from.toHex());
+  if (from === null) {
+    // Owner - as Sender
+    from = new Owner(event.params.from.toHex());
+    from.totalTokens = BigInt.zero();
+    from.totalTokensMinted = BigInt.zero();
+    from.totalTransactions = BigInt.zero();
+    from.block = event.block.number;
+    from.createdAt = event.block.timestamp;
+    from.updatedAt = event.block.timestamp;
+    from.save();
+  }
 
-		token.owner    = to.id
-		token.approval = fetchAccount(Address.zero()).id // implicit approval reset on transfer
-
-		contract.save()
-		token.save()
-
-		let ev         = new ERC721Transfer(events.id(event))
-		ev.emitter     = contract.id
-		ev.transaction = transactions.log(event).id
-		ev.timestamp   = event.block.timestamp
-		ev.contract    = contract.id
-		ev.token       = token.id
-		ev.from        = from.id
-		ev.to          = to.id
-		ev.save()
+  if (event.params.from.equals(Address.zero())) {
+  } else {
+	if (from.totalTokens === BigInt.fromI32(1)) { // if sender has only one token before transaction
+		collection.totalOwners.minus(BigInt.fromI32(1));
+		collection.save()
 	}
+	from.totalTokens.minus(BigInt.fromI32(1));
+  }
+
+//   from.totalTokens = event.params.from.equals(Address.zero())
+//     ? from.totalTokens
+//     : from.totalTokens.minus(BigInt.fromI32(1));
+
+//   if (from.totalTokens === BigInt.fromI32(0)) { // if sender has no token after transaction
+// 	collection.totalOwners.minus(BigInt.fromI32(1));
+// 	collection.save()
+//   }
+
+  from.totalTransactions = from.totalTransactions.plus(BigInt.fromI32(1));
+  from.updatedAt = event.block.timestamp;
+  from.save();
+
+  let to = Owner.load(event.params.to.toHex());
+  if (to === null) {
+    // Owner - as Receiver
+    to = new Owner(event.params.to.toHex());
+    to.totalTokens = BigInt.zero();
+    to.totalTokensMinted = BigInt.zero();
+    to.totalTransactions = BigInt.zero();
+    to.block = event.block.number;
+    to.createdAt = event.block.timestamp;
+    to.updatedAt = event.block.timestamp;
+    to.save();
+  }
+  if (to.totalTokens === BigInt.fromI32(0)) { // if receiver doesn't have any token before transaction
+	collection.totalOwners = collection.totalOwners.plus(BigInt.fromI32(1));
+	collection.save()
+  }
+  to.totalTokens = to.totalTokens.plus(BigInt.fromI32(1));
+  to.totalTransactions = to.totalTransactions.plus(BigInt.fromI32(1));
+  to.updatedAt = event.block.timestamp;
+  to.save();
+
+  let token = Token.load(event.address.toHex() + "-" + event.params.tokenId.toString());
+  if (token === null) {
+    // Token
+    token = new Token(event.address.toHex() + "-" + event.params.tokenId.toString());
+    token.collection = collection.id;
+    token.tokenID = event.params.tokenId;
+    token.tokenURI = fetchTokenURI(event.address, event.params.tokenId);
+    token.minter = to.id;
+    token.owner = to.id;
+    token.burned = false;
+    token.totalTransactions = BigInt.zero();
+    token.block = event.block.number;
+    token.createdAt = event.block.timestamp;
+    token.updatedAt = event.block.timestamp;
+    token.save();
+	
+	// Uncounted Token Transfer
+	collection.totalOwners = collection.totalOwners.plus(BigInt.fromI32(1));
+	collection.save();
+
+    // Owner - as Receiver
+    to.totalTokensMinted = to.totalTokensMinted.plus(BigInt.fromI32(1));
+    to.save();
+
+    // Collection
+    collection.totalTokens = collection.totalTokens.plus(BigInt.fromI32(1));
+    collection.save();
+
+    // Blockchain
+    blockchain.totalTokens = blockchain.totalTokens.plus(BigInt.fromI32(1));
+    blockchain.save();
+  }
+  token.owner = to.id;
+  token.burned = event.params.to.equals(Address.zero());
+  token.totalTransactions = token.totalTransactions.plus(BigInt.fromI32(1));
+  token.updatedAt = event.block.timestamp;
+  token.save();
+
+  // Transaction
+  const transaction = new Transaction(event.transaction.hash.toHex());
+  transaction.hash = event.transaction.hash;
+  transaction.from = from.id;
+  transaction.to = to.id;
+  transaction.collection = collection.id;
+  transaction.token = token.id;
+  transaction.gasLimit = event.transaction.gasLimit;
+  transaction.gasPrice = toBigDecimal(event.transaction.gasPrice, 9);
+  transaction.block = event.block.number;
+  transaction.timestamp = event.block.timestamp;
+  transaction.save();
 }
 
-export function handleApproval(event: ApprovalEvent): void {
-	let contract = fetchERC721(event.address)
-	if (contract != null) {
-		let token    = fetchERC721Token(contract, event.params.tokenId)
-		let owner    = fetchAccount(event.params.owner)
-		let approved = fetchAccount(event.params.approved)
 
-		token.owner    = owner.id // this should not be necessary, owner changed is signaled by a transfer event
-		token.approval = approved.id
 
-		token.save()
-		owner.save()
-		approved.save()
 
-		// let ev = new Approval(events.id(event))
-		// ev.emitter     = contract.id
-		// ev.transaction = transactions.log(event).id
-		// ev.timestamp   = event.block.timestamp
-		// ev.token       = token.id
-		// ev.owner       = owner.id
-		// ev.approved    = approved.id
-		// ev.save()
-	}
+// utils/eip721.ts
+export function fetchName(address: Address): string {
+  const contract = EIP721.bind(address);
+
+  const nameResult = contract.try_name();
+  if (!nameResult.reverted) {
+    return nameResult.value;
+  }
+
+  return "unknown";
 }
 
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-	let contract = fetchERC721(event.address)
-	if (contract != null) {
-		let owner      = fetchAccount(event.params.owner)
-		let operator   = fetchAccount(event.params.operator)
-		let delegation = fetchERC721Operator(contract, owner, operator)
+export function fetchSymbol(address: Address): string {
+  const contract = EIP721.bind(address);
 
-		delegation.approved = event.params.approved
+  const symbolResult = contract.try_symbol();
+  if (!symbolResult.reverted) {
+    return symbolResult.value;
+  }
 
-		delegation.save()
-
-		// 	let ev = new ApprovalForAll(events.id(event))
-		// 	ev.emitter     = contract.id
-		// 	ev.transaction = transactions.log(event).id
-		// 	ev.timestamp   = event.block.timestamp
-		// 	ev.delegation  = delegation.id
-		// 	ev.owner       = owner.id
-		// 	ev.operator    = operator.id
-		// 	ev.approved    = event.params.approved
-		// 	ev.save()
-	}
+  return "unknown";
 }
+
+export function fetchTokenURI(address: Address, tokenID: BigInt): string | null {
+  const contract = EIP721.bind(address);
+
+  const uriResult = contract.try_tokenURI(tokenID);
+  if (!uriResult.reverted) {
+    return uriResult.value;
+  }
+
+  return null;
+}
+
+// helpers/utils.ts
+/**
+ * @param amount amount in BigInt
+ * @param decimals number of decimal (optional)
+ * @notice Parse the amount into a BigDecimal instance expressed in decimals
+ */
+ export function toBigDecimal(amount: BigInt, decimals: i32 = 18): BigDecimal {
+	return amount.divDecimal(
+	  BigInt.fromI32(10)
+		.pow(decimals as u8)
+		.toBigDecimal()
+	);
+  }
+  
+  /**
+   * @param quantity amount in ETH (i32)
+   * @param decimals number of decimal (optional)
+   * @notice Parse the amount into a BigInt instance of the amount of wei.
+   */
+  export function parseEther(amount: i32, decimals: u8 = 18): BigInt {
+	const adjuster = BigInt.fromI32(10).pow(decimals);
+	return BigInt.fromI32(amount).times(adjuster);
+  }
+  
